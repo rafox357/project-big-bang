80b0bd30754068d624ac4ccb757389e7f3cc01de
rafox357 <rafigmar357@gmail.com>
2024-12-16 21:03:58 -0600

    fix: add base URL for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  base: '/project-big-bang',
   integrations: [
     react({
       include: ['**/react/**', '**/accessibility/**', '**/interactive/**']
